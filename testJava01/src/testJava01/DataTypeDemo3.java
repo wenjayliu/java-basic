e74eaf557d18ea085adddb9df38e8b260317306c
wenjayliu <865015063@qq.com>
2017-08-08 22:20:37 +0800

    add  a+1

--- testJava01/src/testJava01/DataTypeDemo3.java
+++ testJava01/src/testJava01/DataTypeDemo3.java
@@ -5,15 +5,35 @@ package testJava01;
  * 
  * */
 public class DataTypeDemo3 {
-	
 	public static void main(String[] args) {
 		
-
+		
+		
 		int x = 3;
 		int y = 4;
 		int z = x + y;
 		
-		System.out.println(z);
 
-	}	
+		
+		System.out.println(z);
+	
+/*
+ * DataTypeDemo7
+ * string value
+ * a  97
+ * A  65
+ * 0  48
+ * 
+ * */		
+		System.out.println('a'+1);
+		
+		
+/*
+ *  "" string 
+ * 
+ * */		
+	}
+	
+	
+
 }
