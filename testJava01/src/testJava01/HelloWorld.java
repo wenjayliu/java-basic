522a97c9bfc1d65713dbfdf423a3029437d9bf8b
wenjayliu <865015063@qq.com>
2017-08-08 20:23:03 +0800

    init program

---
+++ testJava01/src/testJava01/HelloWorld.java
@@ -0,0 +1,10 @@
+package testJava01;
+
+public class HelloWorld {
+    /* 
+     * Hello World
+     */
+    public static void main(String []args) {
+        System.out.println("Hello World"); //  Hello World
+    }
+}
