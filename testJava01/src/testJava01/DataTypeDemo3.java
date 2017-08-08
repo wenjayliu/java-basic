e7bda7742521af19df46a05a1553fa12304448cc
wenjayliu <865015063@qq.com>
2017-08-08 21:48:15 +0800

    init add
    +

---
+++ testJava01/src/testJava01/DataTypeDemo3.java
@@ -0,0 +1,19 @@
+package testJava01;
+
+
+/*
+ * 
+ * */
+public class DataTypeDemo3 {
+	
+	public static void main(String[] args) {
+		
+
+		int x = 3;
+		int y = 4;
+		int z = x + y;
+		
+		System.out.println(z);
+
+	}	
+}
