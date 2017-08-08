1aa5899a360119559313233e8c14e84631b368b6
wenjayliu <865015063@qq.com>
2017-08-08 20:26:24 +0800

    init other project
    the secend all

---
+++ HW/src/hwt.java
@@ -0,0 +1,7 @@
+public class hwt {
+	
+    public static void main(String []args) {
+        System.out.println("Hello World"); //  Hello World
+    }
+    
+}
---
+++ sublimep/sb.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    /* 
+     * ello World
+     */
+    public static void main(String []args) {
+        System.out.println("Hello World"); // Hello World
+    }
+}
